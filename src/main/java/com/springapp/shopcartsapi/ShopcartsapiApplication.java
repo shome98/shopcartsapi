5ac574fd463fe5b60168533489681cc40c8f5403
Sowparna Shome <soupornoshome@gmail.com>
2025-02-10 18:43:28 +0530

    first commit

---
+++ src/main/java/com/springapp/shopcartsapi/ShopcartsapiApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.shopcartsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopcartsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopcartsapiApplication.class, args);
+	}
+
+}
